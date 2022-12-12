83916ee967ab9a02a8c8b0695c229cdbd434852f
Avaya Shrestha <68022642+abhayy13th@users.noreply.github.com>
2022-12-12 10:32:38 +0545

    fetchdata navigation

--- Week5/Week5/EmployeeModel.cs
+++ Week5/Week5/EmployeeModel.cs
@@ -10,12 +10,12 @@ namespace Week5.Week5
     {
       
             public Guid Id { get; set; } = Guid.NewGuid();
-            public string Name { get; set; }
-            public string Address { get; set; }
-            public string Email { get; set; }
-            public string Phone { get; set; }
+            public string? Name { get; set; }
+            public string? Email { get; set; }
+            public string? Address { get; set; }
+            public string? Phone { get; set; }
             
-            public string Position { get; set; }
+            public string? Position { get; set; }
             public DateOnly HireDate { get; set; }
         
     }
